b0f7825850beafccad47898a0e9036828615de96
Charliebob <CharliebobCharliebob0604@gmail.com>
2016-10-11 17:29:26 -0700

    Initial Commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,21 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=0d7737261c80598f1e56d19d607ef9cd&units=imperial';
+
+//0d7737261c80598f1e56d19d607ef9cd
+module.exports = {
+  getTemp: function(location){
+    var encodedLocation = encodeURIComponent(location);
+    var requestURL = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestURL).then(function(res){
+      if(res.data.cod && res.data.message){
+        throw new Error(res.data.message);
+      }else{
+        return res.data.main.temp;
+      }
+    }, function(res){
+      throw new Error(res.data.message);
+    });
+  }
+}
