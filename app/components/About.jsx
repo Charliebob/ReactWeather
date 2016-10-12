165addd6c1bb8336cfaa3e88affb4b40fb737f7b
Charliebob <CharliebobCharliebob0604@gmail.com>
2016-10-11 20:57:53 -0700

    changed about page info

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,8 +4,19 @@ var About = React.createClass({
   render: function(){
     return(
       <div>
-        <h3>About</h3>
-        <p>Welcome to About</p>
+        <h1 className="text-center">About</h1>
+        <p>This is a weather app based on React</p>
+        <p>Here are some of the tools I used</p>
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react">React</a> - This was the
+              javascript framework used.
+          </li>
+          <li>
+            <a href="http://openweathermap.org">Open Weather Forcast API</a> - I used open
+              Weather Forcast API to search for weather data by city name.
+          </li>
+        </ul>
       </div>
     );
   }
--- public/bundle.js
+++ public/bundle.js
@@ -26672,26 +26672,55 @@
 /* 254 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	var React = __webpack_require__(8);
 
 	var About = React.createClass({
-	  displayName: 'About',
+	  displayName: "About",
 
 	  render: function render() {
 	    return React.createElement(
-	      'div',
+	      "div",
 	      null,
 	      React.createElement(
-	        'h3',
-	        null,
-	        'About'
+	        "h1",
+	        { className: "text-center" },
+	        "About"
 	      ),
 	      React.createElement(
-	        'p',
+	        "p",
 	        null,
-	        'Welcome to About'
+	        "This is a weather app based on React"
+	      ),
+	      React.createElement(
+	        "p",
+	        null,
+	        "Here are some of the tools I used"
+	      ),
+	      React.createElement(
+	        "ul",
+	        null,
+	        React.createElement(
+	          "li",
+	          null,
+	          React.createElement(
+	            "a",
+	            { href: "https://facebook.github.io/react" },
+	            "React"
+	          ),
+	          " - This was the javascript framework used."
+	        ),
+	        React.createElement(
+	          "li",
+	          null,
+	          React.createElement(
+	            "a",
+	            { href: "http://openweathermap.org" },
+	            "Open Weather Forcast API"
+	          ),
+	          " - I used open Weather Forcast API to search for weather data by city name."
+	        )
 	      )
 	    );
 	  }
