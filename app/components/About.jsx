2e194469009df8ccde4ba254f641d542e3ab6762
Charliebob <CharliebobCharliebob0604@gmail.com>
2016-10-11 17:54:56 -0700

    change about

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 var About = React.createClass({
   render: function(){
     return(
-      <h3>About</h3>
+      <div>
+        <h3>About</h3>
+        <p>Welcome to About</p>
+      </div>
     );
   }
 });
